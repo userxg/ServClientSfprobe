5533b2fbe24b53153240e0a08178c70c1c4ee094
userxg <numberuserDinfo@gmail.com>
2024-05-03 13:12:34 +0300

    deleted test

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -1,10 +1,41 @@
 #include <iostream>
 #include <SFML/Network.hpp>
+#include <SFML/Graphics.hpp>
+
+#include <imgui.h>
+#include <imgui-SFML.h>
 
 
 int main()
 {
-	
+    sf::RenderWindow window(sf::VideoMode(800, 800), "SFML works!");
+    ImGui::SFML::Init(window);
 
-	return 0;
+    sf::CircleShape shape(200.f, 100);
+    shape.setFillColor(sf::Color::Green);
+    shape.setPosition(200, 200);
+
+    sf::Clock deltaClock;
+    while (window.isOpen())
+    {
+
+        sf::Event event;
+
+        while (window.pollEvent(event))
+        {
+            ImGui::SFML::ProcessEvent(event);
+
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        ImGui::SFML::Update(window, deltaClock.restart());
+
+        window.clear();
+        window.draw(shape);
+        ImGui::SFML::Render(window);
+        window.display();
+    }
+
+    ImGui::SFML::Shutdown();
+    return 0;
 }
